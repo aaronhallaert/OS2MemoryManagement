0c3bb44f4ff031eaa03f8cc029a54dec1cc33846
TiboRigole <tibo.rigole@gmail.com>
2018-04-22 14:46:37 +0200

    eerste check in memorycontroller

--- src/logica/MemoryController.java
+++ src/logica/MemoryController.java
@@ -8,9 +8,20 @@ import io.DataProcessing;
 
 
 public class MemoryController {
+	
+	
 	public static ArrayList<Instructie> instructies=new ArrayList<Instructie>();
+	
 	public static ArrayList<Proces> processen=new ArrayList<Proces>();
+	
+	//hier moeten ook nog een ram, en een virtueel geheugen aangemaakt worden
+	//zorgen dat in de default constructors de juiste dingen qua paging al geset zijn
+	
+	//per uitgevoerde instructie wordt de klok met 1 opgehoogd
+	static int klok = 0;
 	 
+	
+	
 	//leest alles in
 	public static void instantiate() {
 	//String pad="D:\\School\\Industriele Ingenieurswetenschappen\\iiw Ba3\\Semester2\\Besturingssystemen 2\\Memory Management Opdracht\\virtual memory\\Instructions_30_3.xml";
@@ -23,8 +34,50 @@ public class MemoryController {
 		System.out.println(i.toString());
 	}
 	
+	//tijdelijke waarden van de instructie
+		int processId;
+		String operation;
+		int vmAdres;
+
 	//instructies en processen overlopen?
+	for(Instructie i: instructies) {
 	
+		//inlezen van de instructie
+		processId = i.getPid();
+		operation = i.getOperatie();
+		vmAdres   = i.getVirtueelAdres();
+		
+		//effectieve uitvoeren van de instructie
+		switch(operation) {
+		case "Read" :	System.out.println("read");
+						break;
+			
+		case "Write":	System.out.println("write");
+						break;
+			
+		case "Terminate":System.out.println("terminate");
+						break;
+			
+		case "Start":	System.out.println("start");
+						break;
+			
+		default :
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		//nieuwe toestand van het geheugen
+		
+		//instructie uitgevoerd
+		//klok 1 omhoog
+		klok++;
+	}
 	
 	}
 }
